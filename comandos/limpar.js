232f043b14c7c4efeb3fcfd396170c23778a4e98
LockDzn <ryanssouza00@gmail.com>
2018-06-16 20:43:47 -0300

    :inbox_tray: | Novo comando k!limpar

--- comandos/limpar.js
+++ comandos/limpar.js
@@ -19,7 +19,7 @@ module.exports.run = async (client, message, args) =>{
    if(args == "2") return message.reply("por favor, digite o número maior que 2 para deletar as mensagens. :x:").then(msg => msg.delete(8000));
    if(args == "1") return message.reply("por favor, digite o número maior que 2 para deletar as mensagens. :x:").then(msg => msg.delete(8000));
    if(!args[0]) return message.reply("por favor, digite o número maior que 2 para deletar as mensagens.");
-   message.channel.bulkDelete(args[0]).catch(error => message.reply(`Algumas mensagens não puderam ser deletadas por serem enviadas a mais de 2 semanas!`));
+   message.channel.bulkDelete(args[0]).catch(error => message.reply(`algumas mensagens não puderam ser deletadas por serem enviadas a mais de **2 semanas**!`));
    
    message.channel.send(`Chat limpo! Limpado **${args[0]}** mensagens por ${message.author}.`).then(msg => msg.delete(5000));
    
