d54b7d18f12374720cb3ee6ebb6e5e864147ccdd
LockDzn <ryanssouza00@gmail.com>
2018-06-17 20:42:34 -0300

    :arrows_counterclockwise: | Comando k!vote atualizado

--- comandos/vote.js
+++ comandos/vote.js
@@ -16,19 +16,47 @@ module.exports.run = async (client, message, args) =>{
       .addField("Como usar", "`k!vote <mensagem da votação>`")
       .addField("Permissão", "O staff que for usar o comando tem que esta em um cargo com a permissão `Gerenciar servidor`")
    
-   let mensg = args.join(" ");
-   if(!mensg)
-      return message.channel.send(message.author, comousar).then(msg => msg.delete(10000));
+    let mensg = args.join(" ");
+    if(!mensg)
+        return message.channel.send(message.author, comousar).then(msg => msg.delete(10000));
    
-   const vote = new Discord.RichEmbed()
-      .setTitle(":thinking: Votação")
-      .setDescription(`\n\n${mensg}`)
-      .setColor("#00ffa8")
-      .setFooter(`Por: ${message.author.tag} - © Kally Votação`)
-   
-   message.channel.send("@everyone")
-   const m = await message.channel.send(vote)
-   m.react('👍')
-   m.react('👎')
-   m.react('❓')
+    const vote = new Discord.RichEmbed()
+        .setTitle(":thinking: Votação")
+        .setDescription(`\n\n${mensg}`)
+        .setColor("#00ffa8")
+        .setFooter(`Por: ${message.author.tag} - © Kally Votação`)
+
+    const categoria = new Discord.RichEmbed()
+        .setTitle("Escolha uma categoria...")
+        .setDescription("\n\n**🔢 Número**\n`Emojis:` 1⃣ 2⃣ 3⃣\n\n**👍 Like**\n`Emojis:` 👍 👎 ❓")
+        .setColor("#00ffa8")
+        .setFooter(`Por: ${message.author.tag}`)
+    message.channel.send(categoria).then(msg=> {
+        msg.react("🔢").then(r => {
+            msg.react("👍")
+        
+            const num = (reaction, user) => reaction.emoji.name === '🔢' && user.id === message.author.id;
+            const like = (reaction, user) => reaction.emoji.name === '👍' && user.id === message.author.id;
+
+            const numl = msg.createReactionCollector(num, { time: 60000 });
+            const likel = msg.createReactionCollector(like, { time: 60000 });
+
+            numl.on('collect', r=> {
+                msg.delete();
+                message.channel.send(vote).then(msg1=> {
+                    msg1.react("1⃣").then(r => {
+                        msg1.react("2⃣")
+                        msg1.react("3⃣")
+                    })})
+            })
+            likel.on('collect', r=> {
+                msg.delete();
+                message.channel.send(vote).then(msg2=> {
+                    msg2.react("👍").then(r => {
+                        msg2.react("👎")
+                        msg2.react("❓")
+                    })})
+            })
+        })
+    })
 }
