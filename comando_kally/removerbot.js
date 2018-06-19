f93d93d9f174356a7e9b207c4eb99d608d0baef4
LockDzn <ryanssouza00@gmail.com>
2018-06-18 23:42:02 -0300

    :inbox_tray: | Novo comando k+removerbot

--- comando_kally/removerbot.js
+++ comando_kally/removerbot.js
@@ -10,14 +10,38 @@ module.exports.run = async (client, message, args) =>{
 
     let guild = client.guilds.find("name", servidornome);
     if(!guild)
-        return message.reply("por favor, o servidor que você digitou não existe!")
+        return message.reply("por favor, digite um nome valido, o servidor que você digitou não existe!")
 
-    guild.leave()
-    message.reply(`você me removeu do servidor ${servidornome}! :white_check_mark:`)
+    const remover = new Discord.RichEmbed()
+        .setAuthor("Remover bot!", client.user.avatarURL)
 
-    client.channels.get("429844744110211072").send(`**Removido por ${message.author}!**`);
+        .setDescription(`Você realmente quer me tirar do servidor **${servidornome}**?\n\n:white_check_mark: Sim\n:negative_squared_cross_mark: Não`)
+        
+        .setTimestamp()
+        .setFooter(`© Kally`, message.author.avatarURL)
+    
+    message.channel.send(`Você realmente quer me tirar do servidor *${servidornome}*?`).then(msg=> {
+        msg.react("✅").then(r => {
+            msg.react("❎")
+        
+            const sim = (reaction, user) => reaction.emoji.name === '✅' && user.id === message.author.id;
+            const nao = (reaction, user) => reaction.emoji.name === '❎' && user.id === message.author.id;
 
+            const siml = msg.createReactionCollector(sim, { time: 60000 });
+            const naol = msg.createReactionCollector(nao, { time: 60000 });
 
-   
+            siml.on('collect', r=> {
+                msg.delete();
+                message.author.send(`Você me removeu do servidor ${servidornome}! :white_check_mark:`)
+            
+                client.channels.get("458341973955313675").send(`**Removido por ${message.author}!**`);
+                guild.leave()
+            })
+            naol.on('collect', r=> {
+                msg.delete();
+                message.author.send(`Você cancelou a minha remoção do servidor ${servidornome}! :negative_squared_cross_mark:`)
+            })
+        })
+    })
    }
 }
