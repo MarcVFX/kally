8fb6587388ccb2d0b2fa35b42dff093fc823a8a0
LockDzn <ryanssouza00@gmail.com>
2018-06-20 23:46:54 -0300

    :arrows_counterclockwise: | Comando k!userinfo atualizado

--- comandos/userinfo.js
+++ comandos/userinfo.js
@@ -16,7 +16,7 @@ module.exports.run = async (client, message, args) =>{
       .setThumbnail(member.user.avatarURL)
       .addField(':computer: ID:', member.user.id)
       .addField(":video_game: Jogando:", `${member.user.presence.game ? `${member.user.presence.game.name}` : "Não está jogando."}`)
-      .addField(':red_circle: Cargos', member.roles.filter(r => r.id !== message.guild.id).map(roles => `\`${roles.name}\``).join(" **|** ") || "Sem cargo")
+      .addField(':briefcase: Cargos', member.roles.filter(r => r.id !== message.guild.id).map(roles => `\`${roles.name}\``).join(", ") || "\`Sem cargo\`")
       .addField(':inbox_tray: Entrou nesse servidor em:', dateFormat(member.joinedAt, 'dd/mm/yyyy') + " às " + dateFormat(member.joinedAt, 'H:MM'))
       .addField(':inbox_tray: Conta criada em:', dateFormat(member.user.createdAt, 'dd/mm/yyyy') + " às " + dateFormat(member.user.createdAt, 'H:MM'))
       .setColor("#00a4ce")
@@ -26,13 +26,14 @@ module.exports.run = async (client, message, args) =>{
    
    } else {
       
-    let member = message.author   
+    let member = message.member
     const info = new Discord.RichEmbed()
       
     .setTitle(":bust_in_silhouette: Informações de" + ` ${message.author.tag}`)
     .setThumbnail(message.author.avatarURL)
     .addField(':computer: ID:', message.author.id)
     .addField(":video_game: Jogando:", `${message.author.presence.game ? `${message.author.presence.game.name}` : "Não está jogando."}`)
+    .addField(':briefcase: Cargos', member.roles.filter(r => r.id !== message.guild.id).map(roles => `\`${roles.name}\``).join(", ") || "\`Sem cargo\`")
     .addField(':inbox_tray: Entrou nesse servidor em:', dateFormat(message.author.joinedAt, 'dd/mm/yyyy') + " às " + dateFormat(message.author.joinedAt, 'H:MM'))
     .addField(':inbox_tray: Conta criada em:', dateFormat(message.author.createdAt, 'dd/mm/yyyy') + " às " + dateFormat(message.author.createdAt, 'H:MM'))
     .setColor("#00a4ce")
