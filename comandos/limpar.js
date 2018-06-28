0fcc9b210db3304410c0d2eec915c1b288e24d52
LockDzn <ryanssouza00@gmail.com>
2018-06-28 13:57:04 -0300

    :arrows_counterclockwise: | Comando k!limpar atualizado

--- comandos/limpar.js
+++ comandos/limpar.js
@@ -9,16 +9,18 @@ module.exports.run = async (client, message, args) =>{
    const comousar = new Discord.RichEmbed()
       .setAuthor("Kally", client.user.avatarURL)
       .setTitle("k!limpar")
-      .setDescription(`Ira limpar o número de mensagens escolhido.`)
+      .setDescription(`Irá limpar o número de mensagens escolhido.`)
       .setColor("#60d1f6")
       .setFooter("© Kally - kally.glitch.me")
-      .addField("Como usar", "`k!limpar <número de mensagens MAIOR QUE 2>`")
+      .addField("Como usar", "`k!limpar <número maior que 2 e menor que 100>`")
       .addField("Permissão", "O staff que for mutar tem que esta em um cargo com a permissão `Gerenciar mensagens`")
-   
-   if(args == "") return message.channel.send(comousar).then(msg => msg.delete(10000));
-   if(args == "2") return message.reply("por favor, digite o número maior que 2 para deletar as mensagens. :x:").then(msg => msg.delete(8000));
-   if(args == "1") return message.reply("por favor, digite o número maior que 2 para deletar as mensagens. :x:").then(msg => msg.delete(8000));
-   if(!args[0]) return message.reply("por favor, digite o número maior que 2 para deletar as mensagens.");
+
+   const num = args.join(" ");
+
+   if(!num) return message.channel.send(comousar).then(msg => msg.delete(10000));
+   if(isNaN(num) == true) return message.reply("por favor, digite somente números, de 2 a 100. :x:").then(msg => msg.delete(10000));
+   if(num < 2) return message.reply("por favor, digite o número maior que 2 para deletar as mensagens. :x:").then(msg => msg.delete(8000));
+   if(num > 100) return message.reply("por favor, digite o número maior que 2 e menos que 100. :x:").then(msg => msg.delete(8000));
    message.channel.bulkDelete(args[0]).catch(error => message.reply(`algumas mensagens não puderam ser deletadas por serem enviadas a mais de **2 semanas**!`));
    
    message.channel.send(`Chat limpo! Limpado **${args[0]}** mensagens por ${message.author}.`).then(msg => msg.delete(5000));
