be859ec5c42dd741f4a7f12eaa951dc4a92883fc
LockDzn <ryanssouza00@gmail.com>
2018-06-30 17:15:15 -0300

    :arrows_counterclockwise: | Comando k!ban atualizado

--- comandos/ban.js
+++ comandos/ban.js
@@ -2,61 +2,72 @@ const Discord = require("discord.js");
 
 module.exports.run = async (client, message, args) =>{
 
-   message.delete().catch(O_o=>{});
-   if (message.member.hasPermission('BAN_MEMBERS')) {
-   const comousar = new Discord.RichEmbed()
+    message.delete().catch(O_o=>{});
+    if(!message.member.hasPermission('BAN_MEMBERS')) return message.reply("você não tem permissão! :x:").then(msg => msg.delete(6000));
+    const comousar = new Discord.RichEmbed()
       .setAuthor("Kally", client.user.avatarURL)
       .setTitle("k!ban")
-      .setDescription(`Ira banir o usuário mencionado.`)
+      .setDescription(`Irá banir o usuário mencionado.`)
       .setColor("#60d1f6")
       .setFooter("© Kally - kally.glitch.me")
       .addField("Como usar", "`k!ban @usuário <motivo>`")
       .addField("Permissão", "O staff que for mutar tem que esta em um cargo com a permissão `Banir membros`")
-   let member = message.mentions.members.first();
-   if(!member)
-      return message.channel.send(message.author, comousar).then(msg => msg.delete(10000));
-   if(!member.bannable) 
-      return message.reply("Eu não posso banir esse usuário! Ele(a) têm um cargo maior.");
+     let member = message.mentions.members.first();
+    if(!member)
+        return message.channel.send(message.author, comousar).then(msg => msg.delete(10000));
+     if(!member.bannable) 
+        return message.reply("eu não posso banir esse usuário! Ele(a) têm um cargo maior.");
 
-   let motivo = args.slice(1).join(' ');
-   if(!motivo)
-      return message.reply("por favor, indique um motivo para o banimento!").then(msg => msg.delete(6000));
+    let motivo = args.slice(1).join(' ');
+    if(!motivo) motivo = "Não informado";
   
-   await member.ban(`Por: ${message.author.tag} | Motivo: ` + motivo)
-      .catch(error => message.reply(`Desculpa ${message.author} Eu não poderia banir por causa de: ${error}`));
+    await member.ban(`Banido por: ${message.author.tag} | Motivo: ` + motivo)
+        .catch(error => message.reply(`Desculpa ${message.author}, eu não poderia banir por causa de: \```js\n${error}\````));
       
       
-   const banmsg = new Discord.RichEmbed()
-       .setAuthor('Você foi banido!', member.user.avatarURL)
-       .setColor("ff0000")
-
-       .setThumbnail("https://i.imgur.com/ZmWM1UT.png")
-
-       .setTimestamp()
-       .setFooter("© Kallyᴮᴱᵀᴬ Moderação", message.author.avatarURL)
-
-        .addField("Motivo:", motivo)
-
-        .addField("Servidor:", message.guild.name)  
-  
-   member.send(banmsg)   
+    const emojizoioban = client.guilds.get("420316735149965322").emojis.find("name", "zoioBAN");
+    const banmsg = new Discord.RichEmbed()
+        .setTitle(`${message.author.tag} | Banido`)
+        .setDescription(`Você foi banido do servidor **${message.guild.name}**! ${emojizoioban}`)
+        .setColor("#aa0303")
+        .setThumbnail(member.user.avatarURL)
+        .addField("📋 Motivo:", motivo)
+        .setTimestamp()
+        .setFooter("© Kally Moderação")
+     
       
-   const banido = new Discord.RichEmbed()
-       .setAuthor(member.user.tag + ' | Ban', member.user.avatarURL)
-       .setDescription(`${member.user.tag} (ID: ${member.user.id}) não respeitou as regras e foi banido! :worried: `)
-       .setColor("ff0000")
+    const banido = new Discord.RichEmbed()
+        .setTitle(`${message.author.tag} | Banido`)
+        .setDescription(`**${member.user.username}** foi banido do servidor! ${emojizoioban}`)
+        .setColor("#aa0303")
+        .setThumbnail(message.author.avatarURL)
+        .addField("👮 Por:", message.author)
+        .addField("📋 Motivo:", motivo)
+        .setTimestamp()
+        .setFooter("© Kally Moderação - k!convite")
 
-       .setThumbnail("https://i.imgur.com/ZmWM1UT.png")
-
-       .setTimestamp()
-       .setFooter("© Kallyᴮᴱᵀᴬ Moderação", message.author.avatarURL)
-
-        .addField("Motivo:", motivo)
-
-        .addField("Staffer:", message.author)
-        
-    message.channel.send(banido)
+    if(message.guild.channels.find("name", "punidos")){
+        let guild = message.guild.channels.find("name", "punidos");   
+        guild.send(banido).catch(O_o=>{});
+        member.send(banmsg).catch(O_o=>{});
+        message.channel.send(`:white_check_mark: | ${message.author} usuário punido com sucesso!`)
+    }else if(message.guild.channels.find("name", "🚫punidos")){
+        let guild = message.guild.channels.find("name", "🚫punidos");
+        guild.send(banido).catch(O_o=>{});
+        member.send(banmsg).catch(O_o=>{});
+        message.channel.send(`:white_check_mark: | ${message.author} usuário punido com sucesso!`)
+    } else if(message.guild.channels.find("name", "punições")){
+        let guild = message.guild.channels.find("name", "punições");
+        guild.send(banido).catch(O_o=>{});
+        member.send(banmsg).catch(O_o=>{});
+        message.channel.send(`:white_check_mark: | ${message.author} usuário punido com sucesso!`)
+    }else if(message.guild.channels.find("name", "🚫punições")){
+        let guild = message.guild.channels.find("name", "🚫punições");
+        guild.send(banido).catch(O_o=>{});
+        member.send(banmsg).catch(O_o=>{});
+        message.channel.send(`:white_check_mark: | ${message.author} usuário punido com sucesso!`)
     } else {
-      message.reply("você não tem permissão! :x:")
-  }
+        message.channel.send(banido)
+        member.send(banmsg).catch(O_o=>{});
+    }
 }
