f0bee5b0b0690d88ca53d6a8732fc8b5e58006ab
LockDzn <ryanssouza00@gmail.com>
2018-06-30 17:21:33 -0300

    :arrows_counterclockwise: | Comando k!unmute atualizado

--- comandos/unmute.js
+++ comandos/unmute.js
@@ -2,73 +2,85 @@ const Discord = require("discord.js");
 
 module.exports.run = async (client, message, args) =>{
 
-   message.delete().catch(O_o=>{});
-   if (message.member.hasPermission('MANAGE_MESSAGES')) {
-   const comousar = new Discord.RichEmbed()
+    message.delete().catch(O_o=>{});
+    if(!message.member.hasPermission('MANAGE_MESSAGES')) return message.reply("você não tem permissão! :x:").then(msg => msg.delete(6000));
+    const comousar = new Discord.RichEmbed()
       .setAuthor("Kally", client.user.avatarURL)
       .setTitle("k!unmute")
-      .setDescription(`Ira desmutar o usuário mencionado.`)
+      .setDescription(`Irá desmutar o usuário mencionado.`)
       .setColor("#60d1f6")
       .setFooter("© Kally - kally.glitch.me")
       .addField("Como usar", "`k!unmute @usuário`")
-      .addField("Permissão", "O staff que for mutar tem que esta em um cargo com a permissão `Gerenciar mensagens`")
-      
-      
-   let member = message.mentions.members.first();
-   if(!member)
-      return message.channel.send(message.author, comousar).then(msg => msg.delete(10000));
-
-   if (!message.guild.roles.find("name", "Mutado")) {
-      
-      const norole = new Discord.RichEmbed()
-         .setColor("ff0000")
-         .setAuthor('Deu um erro', client.user.avatarURL)
-         
-         .setDescription(`O cargo **Mutado** não foi encontrado. :slight_frown: 
-Crie um cargo com o nome "**Mutado**", assim poderei desmutar o usuário!`)
-      
-      
-         .setTimestamp()
-         .setFooter("© Kallyᴮᴱᵀᴬ ERRO", message.author.avatarURL)
-      
-      message.channel.send(message.author, norole)
-      
-      
-     } else {
-        let role = message.guild.roles.find("name", "Mutado");
-        member.removeRole(role)
-        
-        const unmutemsg = new Discord.RichEmbed()
-             .setAuthor('Você foi desmutado!', member.user.avatarURL)
-             .setColor("ff0000")
-
-             .setThumbnail(member.user.avatarURL)
-
-             .setTimestamp()
-             .setFooter("© Kallyᴮᴱᵀᴬ Moderação", message.author.avatarURL)
+      .addField("Permissão", "O staff que for mutar tem que esta em um cargo com a permissão `Gerenciar Mensagens`")
+    let member = message.mentions.members.first();
+    if(!member)
+        return message.channel.send(message.author, comousar).then(msg => msg.delete(10000));
 
 
-             .addField("Servidor:", message.guild.name)  
-
-        member.send(unmutemsg)
-        
-        const mutado = new Discord.RichEmbed()
-           .setAuthor(member.user.tag + ' | Mute', member.user.avatarURL)
-           .setDescription(`${member.user.tag} (ID: ${member.user.id}) foi desmutado! HEEYY! Agora respeite as regras. :wink: `)
-           .setColor("ff0000")
-
-           .setThumbnail(message.author.avatarURL)
-
-           .setTimestamp()
-           .setFooter("© Kallyᴮᴱᵀᴬ Moderação", message.author.avatarURL)
-
-
-           .addField("Staffer:", message.author)
-        
-        message.channel.send(mutado)
-     }
   
-   } else {
-      message.reply("você não tem permissão! :x:").then(msg => msg.delete(6000));
-  }
+    let muterole = message.guild.roles.find("name", "Mutado");
+    if(!muterole){
+        try {
+            muterole = await message.guild.createRole({
+                name: "Mutado",
+                color: "#000000",
+                permissions: []
+            });
+            message.guild.channels.forEach(async (channel, id) =>{
+                await channel.overwritePermissions(muterole, {
+                    SEND_MESSAGES: false,
+                    ADD_REACTION: false,
+                    CONNECT: false
+                });
+            });
+        } catch (a) {
+            console.error(a.stack);
+        }
+    }
+    
+    member.removeRole(muterole)
+
+    const emojizoioban = client.guilds.get("420316735149965322").emojis.find("name", "zoioBAN");
+    const desmutemsg = new Discord.RichEmbed()
+        .setTitle(`${message.author.tag} | Desmutado`)
+        .setDescription(`Você foi desmutado no servidor **${message.guild.name}**! :smiley:`)
+        .setColor("#aa0303")
+        .setThumbnail(member.user.avatarURL)
+        .setTimestamp()
+        .setFooter("© Kally Moderação")
+     
+      
+    const desmutado = new Discord.RichEmbed()
+        .setTitle(`${message.author.tag} | Desmutado`)
+        .setDescription(`**${member.user.username}** foi desmutado no servidor! :smiley:`)
+        .setColor("#aa0303")
+        .setThumbnail(message.author.avatarURL)
+        .addField("👮 Por:", message.author)
+        .setTimestamp()
+        .setFooter("© Kally Moderação - k!convite")
+        
+    if(message.guild.channels.find("name", "punidos")){
+        let guild = message.guild.channels.find("name", "punidos");   
+        guild.send(desmutado).catch(O_o=>{});
+        member.send(desmutemsg).catch(O_o=>{});
+        message.channel.send(`:white_check_mark: | ${message.author} usuário despunido com sucesso!`)
+    }else if(message.guild.channels.find("name", "🚫punidos")){
+        let guild = message.guild.channels.find("name", "🚫punidos");
+        guild.send(desmutado).catch(O_o=>{});
+        member.send(desmutemsg).catch(O_o=>{});
+        message.channel.send(`:white_check_mark: | ${message.author} usuário despunido com sucesso!`)
+    } else if(message.guild.channels.find("name", "punições")){
+        let guild = message.guild.channels.find("name", "punições");
+        guild.send(desmutado).catch(O_o=>{});
+        member.send(desmutemsg).catch(O_o=>{});
+        message.channel.send(`:white_check_mark: | ${message.author} usuário despunido com sucesso!`)
+    }else if(message.guild.channels.find("name", "🚫punições")){
+        let guild = message.guild.channels.find("name", "🚫punições");
+        guild.send(desmutado).catch(O_o=>{});
+        member.send(desmutemsg).catch(O_o=>{});
+        message.channel.send(`:white_check_mark: | ${message.author} usuário despunido com sucesso!`)
+    } else {
+        guild.send(desmutado).catch(O_o=>{});
+        member.send(desmutemsg).catch(O_o=>{});
+    }
 }
