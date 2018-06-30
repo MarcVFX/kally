c9b10e2b15702e2423e038d52e4cf1fcc8ad5e26
LockDzn <ryanssouza00@gmail.com>
2018-06-30 17:18:30 -0300

    :arrows_counterclockwise: | Comando k!kick atualizado

--- comandos/kick.js
+++ comandos/kick.js
@@ -3,60 +3,71 @@ const Discord = require("discord.js");
 module.exports.run = async (client, message, args) =>{
 
     message.delete().catch(O_o=>{});
-    if(!message.member.hasPermission('KICK_MEMBERS'))
-      return message.reply("você não tem permissão! :x:");
-    
-    const comousarkick = new Discord.RichEmbed()
+    if(!message.member.hasPermission('KICK_MEMBERS')) return message.reply("você não tem permissão! :x:").then(msg => msg.delete(6000));
+    const comousar = new Discord.RichEmbed()
       .setAuthor("Kally", client.user.avatarURL)
       .setTitle("k!kick")
-      .setDescription(`Ira kickar o usuário mencionado.`)
+      .setDescription(`Irá kikar o usuário mencionado.`)
       .setColor("#60d1f6")
       .setFooter("© Kally - kally.glitch.me")
       .addField("Como usar", "`k!kick @usuário <motivo>`")
-      .addField("Permissão", "O staff que for expulsar tem que esta em um cargo com a permissão `Expulsar membros`")
-    
-    let member = message.mentions.members.first();
+      .addField("Permissão", "O staff que for kikar tem que está em um cargo com a permissão `Expulsar membros`")
+     let member = message.mentions.members.first();
     if(!member)
-      return message.channel.send(comousarkick).then(msg => msg.delete(10000));
-    if(!member.kickable) 
-      return message.reply("eu não posso expulsar esse usuário! Ele(a) têm um cargo maior.").then(msg => msg.delete(8000));
-    
+        return message.channel.send(message.author, comousar).then(msg => msg.delete(10000));
+     if(!member.bannable) 
+        return message.reply("eu não posso expulsar esse usuário! Ele(a) têm um cargo maior.");
+
     let motivo = args.slice(1).join(' ');
     if(!motivo) motivo = "Não informado";
-    
-    // Now, time for a swift kick in the nuts!
-    await member.kick(`Por: ${message.author.tag} | Motivo: ` + motivo)
-      .catch(error => message.reply(`Desculpa ${message.author} Eu não poderia banir por causa de: ${error}`));
-    
-   const kickmsg = new Discord.RichEmbed()
-       .setAuthor('Você foi kikado!', member.user.avatarURL)
-       .setColor("ff0000")
-
-       .setThumbnail(member.user.avatarURL)
-   
-       .setTimestamp()
-       .setFooter("© Kallyᴮᴱᵀᴬ Moderação", message.author.avatarURL)
-
-        .addField("Motivo:", motivo)
-
-        .addField("Servidor:", message.guild.name)  
   
-   member.send(kickmsg)
-    
-   const kickado = new Discord.RichEmbed()
-       .setAuthor(member.user.tag + ' | Kick', member.user.avatarURL)
-       .setDescription(`${member.user.tag} (ID: ${member.user.id}) não respeitou as regras e foi kickado! :worried: `)
-       .setColor("ff0000")
-
-       .setThumbnail(member.user.avatarURL)
-
-       .setTimestamp()
-       .setFooter("© Kallyᴮᴱᵀᴬ Moderação", message.author.avatarURL)
-
-        .addField("Motivo:", motivo)
-
-        .addField("Staffer:", message.author)
+    await member.kick(`Expulso por: ${message.author.tag} | Motivo: ` + motivo)
+      .catch(error => message.reply(`Desculpa ${message.author} Eu não poderia banir por causa de: ${error}`));
+      
+      
+    const emojizoioban = client.guilds.get("420316735149965322").emojis.find("name", "zoioBAN");
+    const kickmsg = new Discord.RichEmbed()
+        .setTitle(`${message.author.tag} | Expulso`)
+        .setDescription(`Você foi expulso do servidor **${message.guild.name}**! :worried:`)
+        .setColor("#aa0303")
+        .setThumbnail(member.user.avatarURL)
+        .addField("📋 Motivo:", motivo)
+        .setTimestamp()
+        .setFooter("© Kally Moderação")
+     
+      
+    const kikado = new Discord.RichEmbed()
+        .setTitle(`${message.author.tag} | Expulso`)
+        .setDescription(`**${member.user.username}** foi expulso do servidor! :worried:`)
+        .setColor("#aa0303")
+        .setThumbnail(message.author.avatarURL)
+        .addField("👮 Por:", message.author)
+        .addField("📋 Motivo:", motivo)
+        .setTimestamp()
+        .setFooter("© Kally Moderação - k!convite")
         
-    message.channel.send(kickado)
-
+    if(message.guild.channels.find("name", "punidos")){
+        let guild = message.guild.channels.find("name", "punidos");   
+        guild.send(kikado).catch(O_o=>{});
+        member.send(kickmsg).catch(O_o=>{});
+        message.channel.send(`:white_check_mark: | ${message.author} usuário punido com sucesso!`)
+    }else if(message.guild.channels.find("name", "🚫punidos")){
+        let guild = message.guild.channels.find("name", "🚫punidos");
+        guild.send(kikado).catch(O_o=>{});
+        member.send(kickmsg).catch(O_o=>{})
+        message.channel.send(`:white_check_mark: | ${message.author} usuário punido com sucesso!`)
+    } else if(message.guild.channels.find("name", "punições")){
+        let guild = message.guild.channels.find("name", "punições");
+        guild.send(kikado).catch(O_o=>{});
+        member.send(kickmsg).catch(O_o=>{})
+        message.channel.send(`:white_check_mark: | ${message.author} usuário punido com sucesso!`)
+    }else if(message.guild.channels.find("name", "🚫punições")){
+        let guild = message.guild.channels.find("name", "🚫punições");
+        guild.send(kikado).catch(O_o=>{});
+        member.send(kickmsg).catch(O_o=>{})
+        message.channel.send(`:white_check_mark: | ${message.author} usuário punido com sucesso!`)
+    } else {
+        guild.send(kikado).catch(O_o=>{});
+        member.send(kickmsg).catch(O_o=>{})
+    }
 }
