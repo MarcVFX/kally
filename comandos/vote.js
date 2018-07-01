d7c5a71c26c5ba1d75e96329c8a8b86e29a93994
LockDzn <ryanssouza00@gmail.com>
2018-07-01 11:03:24 -0300

    :arrows_counterclockwise: | Comando k!vote atualizado

--- comandos/vote.js
+++ comandos/vote.js
@@ -10,11 +10,11 @@ module.exports.run = async (client, message, args) =>{
     const comousar = new Discord.RichEmbed()
       .setAuthor("Kally", client.user.avatarURL)
       .setTitle("k!vote")
-      .setDescription(`Ira abrir uma votação no canal que a mensagem foi executada.`)
-      .setColor("#60d1f6")
-      .setFooter("© Kally - kally.glitch.me")
-      .addField("Como usar", "`k!vote <mensagem da votação>`")
-      .addField("Permissão", "O staff que for usar o comando tem que esta em um cargo com a permissão `Gerenciar servidor`")
+      .setDescription(`Irá abrir uma votação no canal que a mensagem foi executada.`)
+      .setColor("#22a7cc")
+      .setFooter("© Kally - k!convite - kally.glitch.me")
+      .addField("Como usar:", "`k!vote <mensagem da votação>`")
+      .addField("Permissão:", "O staff que for usar o comando tem que esta em um cargo com a permissão `Gerenciar servidor`")
    
     let mensg = args.join(" ");
     if(!mensg)
