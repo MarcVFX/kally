2c763fc46d034ca1cf1d75a94607dd69892fdbad
LockDzn <ryanssouza00@gmail.com>
2018-07-08 18:38:31 -0300

    :inbox_tray: | Novo comando k!convite

--- comandos/convite.js
+++ comandos/convite.js
@@ -8,7 +8,7 @@ module.exports.run = async (client, message, args) =>{
         .setColor("#1297f0")
         .setAuthor("Convite", client.user.avatarURL)
         .addField("Convite para servidor de suporte do Kally:", "https://discord.gg/fsSNJJH")
-        .addField("Invite para colocar o Kally no seu servidor:", "https://kally.glitch.me/invite.html")
+        .addField("Invite para colocar o Kally no seu servidor:", "https://kally.glitch.me/invite")
 
       message.author.send(convite).catch(error => message.reply(`ops! Seu privado está desativado e não consigo enviar o convite. :worried:`).then(msg => msg.delete(8000)));
   
